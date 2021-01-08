429caa9d53ce9bde0ec7ba7fe6e09d918267ace9
Adrien Calmesnil <adcalmesnil@gmail.com>
2021-01-08 21:56:44 +0100

    add poubelle logo

--- components/products.js
+++ components/products.js
@@ -1,9 +1,20 @@
 import React, { Component, useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text,Image, View, StyleSheet, Button } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default ({ navigation }) => {
     
+    switch (global.product.packaging) {
+        /*case 'Plastique':
+            const image = <img src="../assets/icon.png" alt="Icon"/>
+            break;*/
+        case 'Carton':
+            const image = <img src='../assets/icon.png'/>
+            break;
+        default:
+            break;
+    }
+
     return (
         <View>
             <Text style={styles.text}>{ global.product.code }</Text>
@@ -11,6 +22,7 @@ export default ({ navigation }) => {
             <Text>{ global.product.product_name }</Text>
             <Text>{ global.product.packaging }</Text>
             <Text>{ global.product.brand }</Text>
+            {image}
             <Button
                 title="Scanner un nouveau code barre"
                 onPress={() => navigation.navigate('Home')}
